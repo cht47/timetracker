1513a0c5e6304916b3404262fbf46426dd7506f8
cht47 <2283180+cht47@users.noreply.github.com>
2025-03-05 23:02:16 +0100

    init upload

---
+++ service-worker.js
@@ -0,0 +1,40 @@
+// service-worker.js
+const CACHE_NAME = "time-tracker-pwa-v1";
+const FILES_TO_CACHE = [
+  "./",
+  "./index.html",
+  "./style.css",
+  "./manifest.json",
+  "./service-worker.js",
+  // ggf. Icons wie ./icon-192.png, ./icon-512.png
+];
+
+self.addEventListener("install", (evt) => {
+  evt.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll(FILES_TO_CACHE);
+    })
+  );
+});
+
+self.addEventListener("activate", (evt) => {
+  evt.waitUntil(
+    caches.keys().then((keyList) => {
+      return Promise.all(
+        keyList.map((key) => {
+          if (key !== CACHE_NAME) {
+            return caches.delete(key);
+          }
+        })
+      );
+    })
+  );
+});
+
+self.addEventListener("fetch", (evt) => {
+  evt.respondWith(
+    caches.match(evt.request).then((response) => {
+      return response || fetch(evt.request);
+    })
+  );
+});
